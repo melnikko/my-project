901d2cb6619e31d5325b0a0d53c50895a1b50636
Kostia <kost1k04kost1k04@gmail.com>
2022-08-30 18:37:35 +0200

    add files to github

---
+++ app.js
@@ -0,0 +1,14 @@
+// i have 10$
+const amount = 10;
+
+const cookiePrice = 7;
+const candyPrice = 2;
+
+rest = amount - cookiePrice;
+console.log('Amount is', rest);
+
+if (rest >= candyPrice) {
+    console.log('I have enough money');
+} else {
+    console.log('I do not have enough money');
+}
