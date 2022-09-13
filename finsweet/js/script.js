1199893e898c1fe255071eb4fcf7b2b1e9eada4e
Kostia <kost1k04kost1k04@gmail.com>
2022-09-13 19:41:52 +0200

    about section is added

---
+++ finsweet/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let number = 5;
+const leftBorderWisth = 1;
+number = 10;
+console.log(number);
+
+let obj = {
+  name: 'Kostia',
+  surname: 'Melnyk',
+  married: true,
+  age: 26
+};
+
+console.log(obj.name + ' ' + obj.surname);
+
+let arr = ['first', 'second', 'third'];
+console.log(arr[1]);
